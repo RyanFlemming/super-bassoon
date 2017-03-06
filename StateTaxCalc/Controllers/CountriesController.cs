3dc16c8968a9d6bc841ba11ba0c9254087384468
Ryan <rmf919@live.com>
2017-03-06 19:22:09 +1100

    Added Countries Controller

---
+++ StateTaxCalc/Controllers/CountriesController.cs
@@ -0,0 +1,77 @@
+﻿using ERD.Models;
+using StateTaxCalc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace StateTaxCalc.Controllers
+{
+    public class CountriesController : Controller
+    {
+		private IAddressRepo _myRepo;
+
+		public CountriesController() : this(new AddressRepo())
+		{
+
+		}
+		public CountriesController(IAddressRepo repo)
+		{
+			_myRepo = repo;
+		}
+		public ActionResult Index()
+		{
+			AddressModel MyAddressModel = new AddressModel();
+			MyAddressModel.AvailableCountries.Add(new SelectListItem { Text = "Select a Country", Value = "Select item" });
+			var countries = _myRepo.GetAllCountries();
+			foreach (var country in countries)
+			{
+				MyAddressModel.AvailableCountries.Add(new SelectListItem()
+					{
+						Text = country.Name,
+						Value = country.Id.ToString()
+					});
+			}
+			return View(MyAddressModel);
+		}
+
+		// This is the Action we'll call with Ajax
+		// This action only responds to GET requests
+		[AcceptVerbs(HttpVerbs.Get)]
+		public ActionResult GetStatesByCountryId(string countryId)
+		{
+			if (String.IsNullOrEmpty(countryId))
+			{
+				throw new ArgumentNullException("countryId");
+			}
+			int id = 0;
+			bool isValid = Int32.TryParse(countryId, out id);
+			IList<State> states = _myRepo.GetAllStatesByCountryId(id);
+			var result = (from s in states
+						  select new
+						  {
+							  id = s.Id,
+							  name = s.Name,
+							  tax = s.Tax.ToString()
+						  }).ToList();
+			return Json(result, JsonRequestBehavior.AllowGet);
+		}
+
+		[AcceptVerbs(HttpVerbs.Get)]
+		public ActionResult GetTaxRate(string stateId)
+		{
+			if (string.IsNullOrEmpty(stateId))
+			{
+				throw new ArgumentNullException("stateId");
+			}
+			int id = 0;
+			bool isValid = Int32.TryParse(stateId, out id);
+			IList<Decimal> taxRate = _myRepo.GetStateTaxRate(id);
+			var result = taxRate;
+
+			return Json(result, JsonRequestBehavior.AllowGet);
+		}
+    }
+}
